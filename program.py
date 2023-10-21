89907dddb2be65ac284641d81b2ad2226d9d57cc
arzgodka <arosochacka6@gmail.com>
2023-10-21 14:19:56 +0200

    kobieta powyzej 40 z USA gratis marlboro

--- program.py
+++ program.py
@@ -9,4 +9,13 @@ def main():
         print("Błedna wartość.")
 
 
+def sprawdzRegion(region,wiek):
+    try:
+        if(region == 'USA' and int(wiek)>=40):
+            print("Darmowe marlboraski!")
+        else:
+            pass
+    except:
+        print("Wystapił błąd podczas sprawdzania regionu")
+
 main()
